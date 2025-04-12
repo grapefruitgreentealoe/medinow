4914deb10856827328862c7732af6230fa2f8dea
SoHyun <staywithjanejane@gmail.com>
2025-04-13 00:13:44 +0900

    fix: dev와 merge 후 오류 수정정

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { CareUnitModule } from './modules/care-units/care-unit.module';
+import { AuthModule } from './modules/auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env.backend'],
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig.options),
+    UsersModule,
+    CareUnitModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
