1b588cbf67ec3d166374080d04e47fc0da82d654
SoHyun <staywithjanejane@gmail.com>
2025-04-12 21:17:29 +0900

    refactor: response-care-unit.dto 개선

--- apps/backend/src/modules/care-units/dto/response-care-unit.dto.ts
+++ apps/backend/src/modules/care-units/dto/response-care-unit.dto.ts
@@ -1,27 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class OperatingHours {
+  @ApiProperty({ description: '개점 시간' })
+  open: string;
+
+  @ApiProperty({ description: '폐점 시간' })
+  close: string;
+}
+
 export class ResponseCareUnitDto {
+  @ApiProperty({ description: '약국명' })
   name: string;
+
+  @ApiProperty({ description: '주소' })
   address: string;
+
+  @ApiProperty({ description: '전화번호' })
   tel: string;
+
+  @ApiProperty({ description: '기관ID' })
   hpid: string;
+
+  @ApiProperty({ description: '위도' })
   lat: number;
+
+  @ApiProperty({ description: '경도' })
   lng: number;
-  mondayOpen: string;
-  mondayClose: string;
-  tuesdayOpen: string;
-  tuesdayClose: string;
-  wednesdayOpen: string;
-  wednesdayClose: string;
-  thursdayOpen: string;
-  thursdayClose: string;
-  fridayOpen: string;
-  fridayClose: string;
-  saturdayOpen: string;
-  saturdayClose: string;
-  sundayOpen: string;
-  sundayClose: string;
-  holidayOpen: string;
-  holidayClose: string;
-  is_badged: boolean;
-  now_open: boolean;
-  kakao_url: string | null;
+
+  @ApiProperty({ description: '월요일 운영시간' })
+  monday: OperatingHours;
+
+  @ApiProperty({ description: '화요일 운영시간' })
+  tuesday: OperatingHours;
+
+  @ApiProperty({ description: '수요일 운영시간' })
+  wednesday: OperatingHours;
+
+  @ApiProperty({ description: '목요일 운영시간' })
+  thursday: OperatingHours;
+
+  @ApiProperty({ description: '금요일 운영시간' })
+  friday: OperatingHours;
+
+  @ApiProperty({ description: '토요일 운영시간' })
+  saturday: OperatingHours;
+
+  @ApiProperty({ description: '일요일 운영시간' })
+  sunday: OperatingHours;
+
+  @ApiProperty({ description: '공휴일 운영시간' })
+  holiday: OperatingHours;
 }
