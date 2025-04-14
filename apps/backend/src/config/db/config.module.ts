786bd29316ed3d7ef85a1454f5d9ea79fba3dcfa
FrostYOON <yys1423@naver.com>
2025-04-14 21:13:13 +0900

    환경 변수 파일 경로 및 Dockerfile 수정
    - Dockerfile에서 .env 파일을 출력하도록 추가하여 디버깅 용이성 향상.
    - ConfigModule 및 데이터베이스 설정에서 프로덕션 환경의 .env 파일 경로를 '/app/.env'로 수정하여 일관성 유지.

--- apps/backend/src/config/db/config.module.ts
+++ apps/backend/src/config/db/config.module.ts
@@ -8,7 +8,7 @@ import * as Joi from 'joi';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath:
-        process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+        process.env.NODE_ENV === 'production' ? '/app/.env' : '.env.local',
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
