f83e39499e234f3ed3539a232c4acf65e5465dab
SoHyun <staywithjanejane@gmail.com>
2025-04-16 01:15:41 +0900

    feat: 마이그레이션 파일에서 코드 스타일을 일관되게 수정하고, 'typeorm' 임포트 구문을 통일함. 또한, 'up' 및 'down' 메서드의 쿼리 구문을 정리하여 가독성을 향상시킴.

--- apps/backend/src/migrations/1744696190939-AddimgUrlToUser.ts
+++ apps/backend/src/migrations/1744696190939-AddimgUrlToUser.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddimgUrlToUser1744696190939 implements MigrationInterface {
-    name = 'AddimgUrlToUser1744696190939'
+  name = 'AddimgUrlToUser1744696190939';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "imgUrl" text`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "imgUrl" text`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "imgUrl"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "imgUrl"`);
+  }
 }
