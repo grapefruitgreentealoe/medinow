ae265bdbd58f3bc849a7d69a7f357f994095932a
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-17 03:19:32 +0900

    feat: data interface for careunitdata

--- apps/frontend/features/auth/type.ts
+++ apps/frontend/features/auth/type.ts
@@ -2,13 +2,52 @@ export type SignupData = {
   email: string;
   password: string;
   managerName: string;
-  institutionName: string;
   contact: string;
   address: string;
   businessHourStart: string;
   businessHourEnd: string;
   medicalLicenseNumber: string;
-  institutionType: string;
   terms: boolean;
   businessLicense?: File;
 };
+
+export interface CareUnitData {
+  name: string;
+  address: string;
+  tel: string;
+  hpid: string;
+  lat: 0;
+  lng: 0;
+  monday: {
+    open: string;
+    close: string;
+  };
+  tuesday: {
+    open: string;
+    close: string;
+  };
+  wednesday: {
+    open: string;
+    close: string;
+  };
+  thursday: {
+    open: string;
+    close: string;
+  };
+  friday: {
+    open: string;
+    close: string;
+  };
+  saturday: {
+    open: string;
+    close: string;
+  };
+  sunday: {
+    open: string;
+    close: string;
+  };
+  holiday: {
+    open: string;
+    close: string;
+  };
+}
