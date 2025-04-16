8fd22aa8f9255d892379ba52634f82fd88b6d20f
SoHyun <staywithjanejane@gmail.com>
2025-04-16 22:29:35 +0900

    feat: DepartmentsController 및 DepartmentsService의 테스트를 위한 모킹 추가

--- apps/backend/src/modules/departments/departments.service.spec.ts
+++ apps/backend/src/modules/departments/departments.service.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DepartmentsService } from './departments.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Department } from './entities/department.entity';
+import { Repository } from 'typeorm';
 
 describe('DepartmentsService', () => {
   let service: DepartmentsService;
+  let repository: Repository<Department>;
+
+  const mockRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DepartmentsService],
+      providers: [
+        DepartmentsService,
+        {
+          provide: getRepositoryToken(Department),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<DepartmentsService>(DepartmentsService);
+    repository = module.get<Repository<Department>>(
+      getRepositoryToken(Department),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('repository should be defined', () => {
+    expect(repository).toBeDefined();
   });
 });
