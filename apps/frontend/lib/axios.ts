67ae68d8d741ec7b3507540c66d696acf6a71390
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-17 04:00:34 +0900

    feat: axios instance 수정

--- apps/frontend/lib/axios.ts
+++ apps/frontend/lib/axios.ts
@@ -1,76 +1,8 @@
 import axios from 'axios';
-import { getAccessToken, removeAccessToken } from './token';
-import { refreshAccessToken } from './api/refresh';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, // 예: https://api.example.com
   withCredentials: true, // 쿠키 전송 (refreshToken용)
 });
-
-let isRefreshing = false;
-let failedQueue: {
-  resolve: (token: string) => void;
-  reject: (err: any) => void;
-}[] = [];
-
-const processQueue = (error: any, token: string | null = null) => {
-  failedQueue.forEach((prom) => {
-    if (token) prom.resolve(token);
-    else prom.reject(error);
-  });
-  failedQueue = [];
-};
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = getAccessToken();
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({
-            resolve: (token: string) => {
-              originalRequest.headers['Authorization'] = 'Bearer ' + token;
-              resolve(axiosInstance(originalRequest));
-            },
-            reject: (err) => reject(err),
-          });
-        });
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        const newToken = await refreshAccessToken();
-        if (!newToken) throw new Error('No token');
-
-        originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-        processQueue(null, newToken);
-        return axiosInstance(originalRequest);
-      } catch (err) {
-        processQueue(err, null);
-        removeAccessToken();
-        return Promise.reject(err);
-      } finally {
-        isRefreshing = false;
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default axiosInstance;
