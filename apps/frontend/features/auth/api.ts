0ab40746177c456e1026d5c115b299fc00310a61
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-17 04:48:45 +0900

    feat: auth api 추가

--- apps/frontend/features/auth/api.ts
+++ apps/frontend/features/auth/api.ts
@@ -1,9 +1,5 @@
 import axiosInstance from '@/lib/axios';
-
-interface LoginData {
-  email: string;
-  password: string;
-}
+import { AdminSignupData, LoginData, SignupData } from './type';
 
 export const login = async (data: LoginData) => {
   const res = await axiosInstance.post('/auth/login', data);
@@ -12,5 +8,20 @@ export const login = async (data: LoginData) => {
 
 export const logout = async () => {
   const res = await axiosInstance.post('/auth/logout');
+  return res.data;
+};
+
+export const signup = async (data: SignupData) => {
+  const res = await axiosInstance.post('/auth/signup', data);
+  return res.data;
+};
+
+export const adminSignup = async (data: AdminSignupData) => {
+  const res = await axiosInstance.post('/auth/admin-signup', data);
+  return res.data;
+};
+
+export const checkEmail = async (email: string) => {
+  const res = await axiosInstance.get(`/users/check-email?email=${email}`);
   return res.data;
 };
