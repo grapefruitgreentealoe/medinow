59bf61417dbfe44414a136da0e654889f24c1136
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-17 03:00:46 +0900

    feat: loginform api명세에 맞게 수정

--- apps/frontend/features/auth/ui/LoginForm.tsx
+++ apps/frontend/features/auth/ui/LoginForm.tsx
@@ -8,7 +8,6 @@ import { useLogin } from '../model/useLogin';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Checkbox } from '@/components/ui/checkbox';
 
 type FormData = z.infer<typeof loginSchema>;
 
@@ -26,13 +25,13 @@ export default function LoginForm() {
     },
   });
 
-  const { mutateAsync } = useLogin();
+  const { mutate } = useLogin();
   const router = useRouter();
 
   const onSubmit = async (data: FormData) => {
     try {
-      const result = await mutateAsync(data);
-      localStorage.setItem('accessToken', result.accessToken);
+      const result = await mutate(data);
+      localStorage.setItem('isAdmin', result.isAdmin);
       router.push('/');
     } catch (e) {
       alert((e as Error).message);
@@ -53,12 +52,6 @@ export default function LoginForm() {
       {errors.password && (
         <p className="text-sm text-red-500">{errors.password.message}</p>
       )}
-      <div className="flex items-center gap-2">
-        <Checkbox id="terms" {...register('isAdmin')} />
-        <label htmlFor="terms" className="text-sm">
-          관리자 로그인
-        </label>
-      </div>
       <Button type="submit" className="w-full">
         로그인
       </Button>
