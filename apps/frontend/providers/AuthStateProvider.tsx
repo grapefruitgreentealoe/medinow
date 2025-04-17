7c600dea1ea14c39b0d24fb04723b460b001e205
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-17 20:05:05 +0900

    chore: 줄바꿈 제거

--- apps/frontend/providers/AuthStateProvider.tsx
+++ apps/frontend/providers/AuthStateProvider.tsx
@@ -5,7 +5,6 @@ import { useAuthStore } from '@/store/useAuthStore';
 
 export function AuthStateProvider({ children }: { children: React.ReactNode }) {
   const setAuth = useAuthStore((state) => state.setAuth);
-
   useEffect(() => {
     const isAdmin = localStorage.getItem('isAdmin') === 'true';
     const isLogin = localStorage.getItem('isLogin');
