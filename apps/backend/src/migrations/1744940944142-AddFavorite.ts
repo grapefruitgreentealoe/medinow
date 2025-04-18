278c345f8b18301a46f8644639c46e400d2691cc
FrostYOON <yys1423@naver.com>
2025-04-18 10:50:35 +0900

    style: 코드 포맷팅 및 가독성 향상

--- apps/backend/src/migrations/1744940944142-AddFavorite.ts
+++ apps/backend/src/migrations/1744940944142-AddFavorite.ts
@@ -1,20 +1,33 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddFavorite1744940944142 implements MigrationInterface {
-    name = 'AddFavorite1744940944142'
+  name = 'AddFavorite1744940944142';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "favorites" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" uuid, "care_unit_id" uuid, CONSTRAINT "PK_890818d27523748dd36a4d1bdc8" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_09661eb38361553044b5dc26d5" ON "favorites" ("user_id", "care_unit_id") `);
-        await queryRunner.query(`ALTER TABLE "favorites" ADD CONSTRAINT "FK_35a6b05ee3b624d0de01ee50593" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "favorites" ADD CONSTRAINT "FK_61b9e65dbb6cb8702d2faee2922" FOREIGN KEY ("care_unit_id") REFERENCES "care_unit"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "favorites" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" uuid, "care_unit_id" uuid, CONSTRAINT "PK_890818d27523748dd36a4d1bdc8" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_09661eb38361553044b5dc26d5" ON "favorites" ("user_id", "care_unit_id") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "favorites" ADD CONSTRAINT "FK_35a6b05ee3b624d0de01ee50593" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "favorites" ADD CONSTRAINT "FK_61b9e65dbb6cb8702d2faee2922" FOREIGN KEY ("care_unit_id") REFERENCES "care_unit"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "favorites" DROP CONSTRAINT "FK_61b9e65dbb6cb8702d2faee2922"`);
-        await queryRunner.query(`ALTER TABLE "favorites" DROP CONSTRAINT "FK_35a6b05ee3b624d0de01ee50593"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_09661eb38361553044b5dc26d5"`);
-        await queryRunner.query(`DROP TABLE "favorites"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "favorites" DROP CONSTRAINT "FK_61b9e65dbb6cb8702d2faee2922"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "favorites" DROP CONSTRAINT "FK_35a6b05ee3b624d0de01ee50593"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_09661eb38361553044b5dc26d5"`,
+    );
+    await queryRunner.query(`DROP TABLE "favorites"`);
+  }
 }
