dc349f8b57a46e031947e145608e2768c77285b0
FrostYOON <yys1423@naver.com>
2025-04-19 19:24:58 +0900

    feat: ChatsGateway에 CustomLoggerService 통합 및 테스트 환경 설정 개선

--- apps/backend/src/modules/chats/chats.service.ts
+++ apps/backend/src/modules/chats/chats.service.ts
@@ -6,14 +6,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class ChatsService {
   // private readonly connectedClients = new Map<string, Socket>();
-
   // constructor(
   //   @InjectRepository(ChatRoom)
   //   private readonly chatRoomRepository: Repository<ChatRoom>,
   //   private readonly logger: CustomLoggerService,
   //   private readonly userService: UsersService,
   // ) {}
-
   // handleDisconnect(clientId: string) {
   //   this.logger.log(`Client ${clientId} disconnected`);
   //   this.connectedClients.delete(clientId);
