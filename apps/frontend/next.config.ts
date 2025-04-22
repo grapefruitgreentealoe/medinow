f21a79a5512989bec6ec0d05f4a3db618729b756
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-22 15:18:14 +0900

    chore: 프론트 환경변수 수정, 도커파일 적용

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_KAKAO_REST_API_KEY: '69b2bb8d72109ebea926094b48913345',
     NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: 'c44fa34e526434e6a2ed4a02e1ba8ed3',
   },
