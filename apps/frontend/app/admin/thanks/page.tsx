f981e0f1a04c689c82274a49ff8809f3fe7fcd3f
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-23 19:11:29 +0900

    chore: 전체 페이지 컴포넌트 추가

---
+++ apps/frontend/app/admin/thanks/page.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps } from 'next';
+
+const AdminThanksPage = () => {
+  return <div>Enter</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {},
+  };
+};
+
+export default AdminThanksPage;
