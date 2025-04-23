44fe720abc70cf2c5932519085e671457c98a314
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-23 20:42:41 +0900

    chore: next Image to img

--- apps/frontend/app/not-found.tsx
+++ apps/frontend/app/not-found.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image';
-
 export default function NotFound() {
   return (
     <div className="p-8 flex justify-center items-center h-[100vh] text-center bg-[#377782]">
-      <Image
-        alt="404"
-        src="/404_image.png"
-        className="w-[30vw] min-w-[300px]"
-      />
+      <img src="/404_image.png" className="w-[30vw] min-w-[300px]" />
     </div>
   );
 }
