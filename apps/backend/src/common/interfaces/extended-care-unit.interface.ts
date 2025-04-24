15cc344a00562bd5dcaf736c4d59f0ee46d8b9fa
SoHyun <staywithjanejane@gmail.com>
2025-04-24 16:45:55 +0900

    feat: ExtendedCareUnit 인터페이스 수정 및 CareUnitService에서 departments 처리 방식 개선

--- apps/backend/src/common/interfaces/extended-care-unit.interface.ts
+++ apps/backend/src/common/interfaces/extended-care-unit.interface.ts
@@ -1,6 +1,8 @@
 import { CareUnit } from '../../modules/care-units/entities/care-unit.entity';
 
-export interface ExtendedCareUnit extends CareUnit {
+export interface ExtendedCareUnit
+  extends Omit<CareUnit, 'departments' | 'favorites' | 'reviews'> {
   isFavorite: boolean;
   isChatAvailable: boolean;
+  departments: string[];
 }
