2f02c07a3472fd752d3db6a429f26d0a0ea876d0
SoHyun <staywithjanejane@gmail.com>
2025-04-24 23:12:39 +0900

    fix: dotenv.config의 형식을 개선하여 가독성을 높임

--- apps/backend/src/config/typeorm.config.ts
+++ apps/backend/src/config/typeorm.config.ts
@@ -4,7 +4,8 @@ import { DbConfigService } from './db/config.service';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 
-dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
 });
 // 설정 서비스 생성
 const dbConfigService = new DbConfigService(new ConfigService());
