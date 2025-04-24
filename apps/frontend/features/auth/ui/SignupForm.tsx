faa2dd5e2fa0bea8ef3183dee06922d9ad2c651b
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-24 13:20:22 +0900

    feat: 회원가입 시 주소검색 모달

--- apps/frontend/features/auth/ui/SignupForm.tsx
+++ apps/frontend/features/auth/ui/SignupForm.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import LocationSearchModal from '@/shared/ui/LocationSearchModal';
 
 type FormData = z.infer<typeof signupSchema>;
 
@@ -39,6 +40,7 @@ export default function SignupForm() {
 
   const [checking, setChecking] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const onSubmit = async (data: FormData) => {
     setChecking(true);
@@ -60,99 +62,124 @@ export default function SignupForm() {
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="!space-y-6 !max-w-md !mx-auto"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>이메일</FormLabel>
-              <FormControl>
-                <Input placeholder="example@email.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="!space-y-6 !max-w-md !mx-auto"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이메일</FormLabel>
+                <FormControl>
+                  <Input placeholder="example@email.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>비밀번호</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="비밀번호 입력" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="비밀번호 입력"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>이름</FormLabel>
-              <FormControl>
-                <Input placeholder="홍길동" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이름</FormLabel>
+                <FormControl>
+                  <Input placeholder="홍길동" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="nickname"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>닉네임</FormLabel>
-              <FormControl>
-                <Input placeholder="사용할 닉네임" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="nickname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>닉네임</FormLabel>
+                <FormControl>
+                  <Input placeholder="사용할 닉네임" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="address"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>주소</FormLabel>
-              <FormControl>
-                <Input placeholder="읍/면/동 단위 주소" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>주소</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="읍/면/동 단위 주소"
+                    {...field}
+                    readOnly
+                    onClick={() => setModalOpen(true)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="age"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>나이 (선택)</FormLabel>
-              <FormControl>
-                <Input placeholder="숫자 입력" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>나이 (선택)</FormLabel>
+                <FormControl>
+                  <Input placeholder="숫자 입력" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button type="submit" disabled={checking || loading} className="w-full">
-          {checking ? '이메일 확인 중...' : '회원가입'}
-        </Button>
-      </form>
-    </Form>
+          <Button
+            type="submit"
+            disabled={checking || loading}
+            className="w-full"
+          >
+            {checking ? '이메일 확인 중...' : '회원가입'}
+          </Button>
+        </form>
+      </Form>
+      <LocationSearchModal
+        title="주소 검색"
+        subtitle="키워드를 통해 검색하세요"
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onSelect={(data) => {
+          form.setValue('address', data.address);
+          setModalOpen(false);
+        }}
+      />
+    </>
   );
 }
