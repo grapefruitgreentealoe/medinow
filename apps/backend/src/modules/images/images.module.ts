29d2f3194d9bb7cc96c3846c4bcc345bd8f14976
FrostYOON <yys1423@naver.com>
2025-04-25 17:54:42 +0900

    feat: 이미지 모듈 순환 참조 수정

--- apps/backend/src/modules/images/images.module.ts
+++ apps/backend/src/modules/images/images.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,7 +7,11 @@ import { S3Module } from '../s3/s3.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image]), S3Module, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Image]),
+    S3Module,
+    forwardRef(() => UsersModule),
+  ],
   controllers: [ImagesController],
   providers: [ImagesService],
   exports: [ImagesService],
