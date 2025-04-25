58befc00b71e6386c0d9ef0ebf2d1281ac88745f
FrostYOON <yys1423@naver.com>
2025-04-25 16:51:23 +0900

    feat: 이미지 업로드 시 사용자 프로필 검증 및 연결 로직 추가

---
+++ apps/backend/src/migrations/1745567400770-Updateimage.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Updateimage1745567400770 implements MigrationInterface {
+    name = 'Updateimage1745567400770'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_96514329909c945f10974aff5f8"`);
+        await queryRunner.query(`ALTER TABLE "images" DROP COLUMN "userId"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "images" ADD "userId" uuid`);
+        await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_96514329909c945f10974aff5f8" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+    }
+
+}
