7908af5910fc3d85d3330b1267a2c065d4bbc5a2
SoHyun <staywithjanejane@gmail.com>
2025-04-25 11:35:40 +0900

    fix: 즐겨찾기 서비스에서 사용자 즐겨찾기 조회 시 careUnit 존재 여부 필터링 및 사용자 정보 추가

---
+++ apps/backend/src/migrations/1745548200864-AddCascadeFavorites.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddCascadeFavorites1745548200864 implements MigrationInterface {
+    name = 'AddCascadeFavorites1745548200864'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_d7b2ca5e4f6ef01b772bd4c5a0"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_d7b2ca5e4f6ef01b772bd4c5a0" ON "favorites" ("careUnitId", "userId") `);
+    }
+
+}
