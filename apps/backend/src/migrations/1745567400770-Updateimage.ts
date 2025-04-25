7813762730c4abb2efd512bb3cbbe940d00c4546
FrostYOON <yys1423@naver.com>
2025-04-25 16:51:57 +0900

    refactor: Update image migration for improved code formatting and clarity

--- apps/backend/src/migrations/1745567400770-Updateimage.ts
+++ apps/backend/src/migrations/1745567400770-Updateimage.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Updateimage1745567400770 implements MigrationInterface {
-    name = 'Updateimage1745567400770'
+  name = 'Updateimage1745567400770';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_96514329909c945f10974aff5f8"`);
-        await queryRunner.query(`ALTER TABLE "images" DROP COLUMN "userId"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "images" DROP CONSTRAINT "FK_96514329909c945f10974aff5f8"`,
+    );
+    await queryRunner.query(`ALTER TABLE "images" DROP COLUMN "userId"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "images" ADD "userId" uuid`);
-        await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_96514329909c945f10974aff5f8" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "images" ADD "userId" uuid`);
+    await queryRunner.query(
+      `ALTER TABLE "images" ADD CONSTRAINT "FK_96514329909c945f10974aff5f8" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
+    );
+  }
 }
