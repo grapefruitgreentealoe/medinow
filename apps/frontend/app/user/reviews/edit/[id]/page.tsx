7f9f5f50276436a623dd48c3fbc199a369d13658
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-27 14:42:47 +0900

    chore: atom 값 없을 때 잘못된 접근 추가

--- apps/frontend/app/user/reviews/edit/[id]/page.tsx
+++ apps/frontend/app/user/reviews/edit/[id]/page.tsx
@@ -11,6 +11,8 @@ import { createStore, Provider, useAtomValue } from 'jotai';
 import { ReviewData, UpdateReviewInput } from '@/features/user-review/type';
 import { toast } from 'sonner';
 import { ROUTES } from '@/shared/constants/routes';
+import { SearchCareUnitForReview } from '@/features/user-review/ui/SearchCareUnitForReview';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default function EditReviewPage() {
   const { id } = useParams(); // reviewId
@@ -18,6 +20,10 @@ export default function EditReviewPage() {
   const [departments, setDepartments] = useState<
     { id: string; name: string }[] | null
   >(null);
+
+  if (reviewAtomData == null) {
+    return <div>잘못된 접근입니다</div>;
+  }
 
   const defaultValues = {
     content: reviewAtomData.content,
@@ -50,11 +56,29 @@ export default function EditReviewPage() {
     }
   };
   return (
-    <ReviewForm
-      defaultValues={defaultValues}
-      onSubmit={handleSubmit}
-      careUnit={careUnit}
-      departments={departments}
-    />
+    <main className="min-h-screen flex flex-col items-center !px-4 !py-10">
+      <div className="w-full max-w-xl !space-y-10">
+        <Card>
+          <CardContent className="!space-y-6">
+            {/* 제목 */}
+            <div className="text-center !space-y-2">
+              <h1 className="text-2xl font-bold tracking-tight">
+                리뷰 작성하기
+              </h1>
+              <p className="text-muted-foreground text-sm">
+                방문하신 병원에 대한 솔직한 후기를 남겨주세요.
+              </p>
+            </div>
+
+            <ReviewForm
+              defaultValues={defaultValues}
+              onSubmit={handleSubmit}
+              careUnit={careUnit}
+              departments={departments}
+            />
+          </CardContent>
+        </Card>
+      </div>
+    </main>
   );
 }
