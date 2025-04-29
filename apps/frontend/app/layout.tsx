66f434441bce264c52ca206afd8820120b3ed74c
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-29 22:03:25 +0900

    chore:  팀명 추가

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -48,8 +48,9 @@ export default async function RootLayout({
         <main className="flex-1 !pt-[61px]">{children}</main>
 
         {/* 푸터 추가 */}
-        <footer className="fixed bottom-0 left-0 w-full border-t text-center text-sm text-muted-foreground !py-6 bg-white z-50 shadow-md">
-          © 2025 MediNow. All rights reserved.
+        <footer className="fixed bottom-0 left-0 w-full bg-white border-t shadow-md !py-6 text-center text-sm text-muted-foreground z-50">
+          <div>© 2025 MediNow. All rights reserved.</div>
+          <div className="mt-1 text-xs">Created by 삼시세코</div>
         </footer>
 
         <Toaster position="bottom-center" duration={1000} />
