7cdf2d4f79fd8f6561b27f4aa5eef64c554933ee
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-04-30 03:20:41 +0900

    feat: 선택된 채팅방 표시

--- apps/frontend/features/chat/ui/ChatRoomList.tsx
+++ apps/frontend/features/chat/ui/ChatRoomList.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { RoomInfo } from '@/features/chat/type';
+import { cn } from '@/lib/utils';
 import { CareUnit } from '@/shared/type';
 
 interface ChatRoomListProps {
+  selectedUnitId: string;
   rooms: RoomInfo[];
   onSelectRoom: (payload: { roomId: string; selectedUnitId: string }) => void;
   isAdmin?: boolean;
 }
 
 export function ChatRoomList({
+  selectedUnitId,
   rooms,
   onSelectRoom,
   isAdmin = false,
@@ -25,7 +28,10 @@ export function ChatRoomList({
               selectedUnitId: room.careUnitId!,
             })
           }
-          className="w-full p-3 bg-muted rounded text-left hover:bg-accent"
+          className={cn(
+            'w-full p-3 bg-muted rounded text-left hover:bg-accent',
+            selectedUnitId === room.careUnitId ? 'bg-accent' : 'bg-muted'
+          )}
         >
           <div className="font-semibold">
             {isAdmin ? room.user.nickName : room.careUnitName}
