446d4c06f7f8666e70d3ba28d661e64f49c5941c
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-05-02 17:49:09 +0900

    Update next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_KAKAO_REST_API_KEY: '69b2bb8d72109ebea926094b48913345',
-    NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: 'c44fa34e526434e6a2ed4a02e1ba8ed3',
+    NEXT_PUBLIC_KAKAO_REST_API_KEY: '',
+    NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: '',
   },
 };
 export default nextConfig;
