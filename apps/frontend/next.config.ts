911163db5ccba81555fe81752a2a6a25db23d311
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-05-06 21:57:04 +0900

    chore: nextConfig 수정

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_KAKAO_REST_API_KEY: '',
-    NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: '',
-  },
-};
+const nextConfig: NextConfig = {};
 export default nextConfig;
